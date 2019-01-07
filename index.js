daeb63e5dd223494441490d66ea635befc087f30
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2019-01-07 17:35:37 +0700

    Completed nav bar

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const pug = require('pug')
 const app = express()
 const port = 3000
 
-const index = pug.compileFile('./templates/index.pug')
+const index = pug.compileFile('./templates/todo.pug')
 
-app.get('/', (req, res) => res.send(index( {
+app.get('/', (req, res) => res.send(index({
+    title: 'Home',
     text: 'Hello world'
+})))
+
+app.get('/completed', (req, res) => res.send(index({
+    title: 'Completed',
+    text: 'This is completed page'
+})))
+
+app.get('/add', (req, res) => res.send(index({
+    title: 'Add',
+    text: 'This is add page'
 })))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
