5c1222beb2230922d5b1c112778c052498b2b1c0
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2019-01-07 16:27:19 +0700

    Makes pug work

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const pug = require('pug')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const index = pug.compileFile('./templates/index.pug')
+
+app.get('/', (req, res) => res.send(index( {
+    text: 'Hello world'
+})))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
