ea4f33d688c98692d87ab38a017a60ab220b2f34
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2019-01-07 21:35:14 +0700

    Saperate template lender modele

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require('express')
-const pug = require('pug')
+const template = require('./template')
 const app = express()
 const port = 3000
 
-const index = pug.compileFile('./templates/todo.pug')
+app.get('/', (req, res) => {
+    res.send(template.todo({
+        title: 'Home',
+        text: 'Hello world'
+    }))
+})
 
-app.get('/', (req, res) => res.send(index({
-    title: 'Home',
-    text: 'Hello world'
-})))
+app.get('/completed', (req, res) => {
+    res.send(template.completed({
+        title: 'Completed',
+        text: 'This is complete page'
+    }))
+})
 
-app.get('/completed', (req, res) => res.send(index({
-    title: 'Completed',
-    text: 'This is completed page'
-})))
-
-app.get('/add', (req, res) => res.send(index({
-    title: 'Add',
-    text: 'This is add page'
-})))
+app.get('/add', (req, res) => {
+    res.send(template.add({
+        title: 'Add',
+        text: 'This is add page'
+    }))
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ template.js
@@ -0,0 +1,14 @@
+const path = require('path')
+const pug = require('pug')
+
+const files = ['add', 'completed', 'todo']
+const templateDir = path.join(__dirname, 'templates')
+
+const compiled = {}
+
+files.forEach(v => {
+    const templateFile = path.join(templateDir, `${v}.pug`)
+    compiled[v] = pug.compileFile(templateFile)
+})
+
+module.exports = compiled
