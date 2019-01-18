cc983988a50c09ef94635e2f59bdc0b35e6d9195
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2019-01-18 23:49:12 +0700

    Sort when data update

--- static/js/main.js
+++ static/js/main.js
@@ -21,12 +21,16 @@ var todoModel = (function() {
         return res
     }
 
+    function sortTodos(a, b) {
+        return new Date(b.dueDate) - new Date(a.dueDate)
+    }
+
     return {
-        /* For testing purpose only
+        //For testing purpose only
         getTodos: function() {
             return todos
         },
-        */
+
 
         getNotCompleted: function() {
             return getTodo(false)
@@ -45,6 +49,7 @@ var todoModel = (function() {
             }
             var todo = new Todo(id, header, msg, createDate, dueDate)
             todos.push(todo)
+            todos.sort(sortTodos)
             return todo
         },
 
@@ -64,28 +69,43 @@ var todoView = (function() {
     var DOM = {
         selector: {
             nearestData: 'p',
-            nearestDelete: 'a'
+            nearestDelete: 'a',
+            todo: '.todo'
         },
         id: {
             content: 'content'
         },
         data: {
             id: 'data-id'
+        },
+        classes: {
+            todo: 'todo'
         }
     }
 
-    var dateStr = function(date) {
+    function dateStr(date) {
         return `${date.getDay()}/${date.getMonth() + 1}/${date.getFullYear()}`
+    }
+
+    function clear() {
+        var todos = document.querySelectorAll(DOM.selector.todo)
+        todos.forEach(function(e) {e.parentNode.removeChild(e)})
     }
 
     return {
         addTodo: function(todo) {
-            var insertStr = `<${DOM.selector.nearestData} ${DOM.data.id}="${todo.id}"><b>${todo.header}</b>&emsp;${todo.msg}&emsp;<i>Created: ${dateStr(todo.createDate)} Due: ${dateStr(todo.dueDate)}</i>&emsp;<${DOM.selector.nearestDelete} href="#">[delete]</${DOM.selector.nearestDelete}></${DOM.selector.nearestData}>`
+            var insertStr = `<${DOM.selector.nearestData} class="${DOM.classes.todo}" ${DOM.data.id}="${todo.id}"><b>${todo.header}</b>&emsp;${todo.msg}&emsp;<i>Created: ${dateStr(todo.createDate)} Due: ${dateStr(todo.dueDate)}</i>&emsp;<${DOM.selector.nearestDelete} href="#">[delete]</${DOM.selector.nearestDelete}></${DOM.selector.nearestData}>`
             document.getElementById(DOM.id.content).insertAdjacentHTML('afterbegin', insertStr)
         },
 
         getDom: function() {
             return DOM
+        },
+
+        update: function(todos) {
+            clear()
+            var _this = this
+            todos.forEach(function(e) {_this.addTodo(e)})
         },
     }
 
@@ -95,7 +115,7 @@ var todoController = (function(model, view) {
 
     var DOM = view.getDom()
 
-    var parseDate = function(dateStr) {
+    function parseDate(dateStr) {
         var dateArr = dateStr.split(/(?:\/|-|\s)/)
         var date = dateArr[0]
         var month = dateArr[1]
@@ -105,21 +125,26 @@ var todoController = (function(model, view) {
         return d
     }
 
-    var addViewEvent = function() {
+    function updateView() {
+        var todos = model.getNotCompleted()
+        view.update(todos)
+    }
+
+    function addViewEvent() {
         document.getElementById(DOM.id.content).addEventListener('click', (e) => {
             if(e.target.tagName.toUpperCase() === DOM.selector.nearestDelete.toUpperCase()) {
-            var closest = e.target.closest(DOM.selector.nearestData)
-            var id = closest.getAttribute(DOM.data.id)
+                var closest = e.target.closest(DOM.selector.nearestData)
+                var id = closest.getAttribute(DOM.data.id)
                 model.deleteTodo(id)
-                closest.parentNode.removeChild(closest)
+                updateView()
             }
         })
     }
 
     return {
         makeTodo: function(header, msg, dueDate) {
-            var todo = model.putTodo(header, msg, new Date(), parseDate(dueDate))
-            view.addTodo(todo)
+            model.putTodo(header, msg, new Date(), parseDate(dueDate))
+            updateView()
         },
 
         init: function() {
